802efe94d25255e25f9f17a9e4119d75a1646756
rockstar2514 <56267835+rockstar2514@users.noreply.github.com>
2020-02-01 22:14:49 +0530

    Create recurrence.cpp

---
+++ recurrence.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int level(int n)
+{
+if(n!=1)
+{
+int sum=0;
+
+for(int i=n-1;i>=1;i++)
+{
+sum=sum+level(i);
+}
+}
+else
+{
+return 1;
+}
+}
+
+int main()
+{
+
+level[100];
+
+
+}
